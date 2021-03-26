54a1ab5f233d7c869314400948122e39ec336301
mattsnow <35243275+Rooseveltthedev@users.noreply.github.com>
2021-03-25 21:49:23 -0400

    Add files via upload

---
+++ website/hvac.js
@@ -0,0 +1,27 @@
+﻿var temp = 70;
+
+function incTemp() {
+    temp += 1;
+    document.getElementById("p3").innerText = temp + String.fromCharCode(176);
+}
+
+function decTemp() {
+    temp -= 1;
+    document.getElementById("p3").innerText = temp + String.fromCharCode(176);
+}
+
+function cooling() {
+    document.getElementById("p2").innerText = "Currently:\n" + "Cooling";
+}
+function emrg() {
+    document.getElementById("p2").innerText = "Currently:\n" + "EMRG";
+}
+function auto() {
+    document.getElementById("p2").innerText = "Currently:\n" + "Auto";
+}
+function heating() {
+    document.getElementById("p2").innerText = "Currently:\n" + "Heating";
+}
+function off() {
+    document.getElementById("p2").innerText = "Currently:\n" + "Off";
+}
